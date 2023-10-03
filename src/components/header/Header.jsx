69578ca194acff459b7c26aa718783c33d30b6b0
ApiiiG <apcag.hrvoje@gmail.com>
2023-10-03 14:04:20 +0200

    d
    Portfolio test commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'am </h5>
+        <h1>Hrvoje</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA/>
+      </div>
+
+      <div className="me">
+        <img src={ME} alt="" />
+      </div>
+
+      <a href="#contact" className='scroll_down'>Scroll Down</a>
+    </header>
+    )
+}
+
+export default Header
